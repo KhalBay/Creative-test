69bb3d2a0b889198c61c8e834d30dc0c67975513
KhalBay <egorczech7@gmail.com>
2021-04-07 17:58:22 +0500

    top&left menu

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var $toggleButton = $('.toggle-button'),
+        $menuWrap = $('.menu-wrap'),
+        $sidebarArrow = $('.sidebar-menu-arrow');
+    $toggleButton.on('click', function() {
+        $(this).toggleClass('button-open');
+        $menuWrap.toggleClass('menu-show');
+    });
+    $sidebarArrow.click(function() {
+        $(this).next().slideToggle(300);
+    });
+});
