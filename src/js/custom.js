5005d77fb17882c93913697f23a6ed38f8acbbfb
Egor <jagyar37@gmail.com>
2021-04-30 10:40:04 +0500

    Final Task 2

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,12 +1,32 @@
-let Number = [1,2,3,4,5];
-let form = document.querySelector('.out_arr')
-let input = document.querySelector('.input_N');
+//start task 1
+
+let number = [1, 2, 3, 4, 5];
+let form = document.querySelector('.form__task1')
 let numberArray = document.querySelector('.numberArray');
 
 form.onsubmit = function (evt) {
     evt.preventDefault();
+    let step = this.number.value;
+    for (i = 0; i < step; i++) number.unshift(number.pop());
+    numberArray.textContent = '[' + number.join() + ']';
+    console.log(number);
+    return false;
+};
+//end task 1
+//start task 2
 
-    Number = Number.slice(-input).concat(Number);
-    numberArray.textContent = '[' + Number;
+let string1 = document.querySelector('.string1')
+let string2 = document.querySelector('.string2')
+
+const finder = (string1, string2) => {
+    arr = [];
+    string1.split('').reduce((last,item)=>{
+        if (string2.indexOf(`${last}${item}`) !== -1) {
+            arr.push(`${last}${item}`);
+            return `${last}${item}`;
+        }
+        else return item;
+    }, '');
+    return arr.sort((a, b)=>b.length-a.length)[0]
+    console.log()
 }
-
